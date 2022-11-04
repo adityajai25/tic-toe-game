6b0bb3663fed32604affcea4c1efccc8f89ddbf0
Aditya JP <adityajai243@gmail.com>
2022-11-05 02:36:30 +0530

    Updated

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var restart = document.querySelector('#res');
 var squares = document.querySelectorAll('td');
-// for  clearing all boxed restarting
 function clearBox() {
   for (var i = 0; i < squares.length; i++) {
     squares[i].textContent = '';
